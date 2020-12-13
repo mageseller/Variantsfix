6ca50ceae650f4a8345de2437d32bc9c25f8b0f4
satish29g1993 <72189328+satish29g1993@users.noreply.github.com>
2020-12-14 02:13:36 +0530

    Added some fixes related to phpcs

--- Plugin/ConfigurableProduct/Block/Product/View/Type/Configurable.php
+++ Plugin/ConfigurableProduct/Block/Product/View/Type/Configurable.php
@@ -3,27 +3,26 @@ namespace Mageseller\Variantsfix\Plugin\ConfigurableProduct\Block\Product\View\T
 
 class Configurable extends \Magento\ConfigurableProduct\Block\Product\View\Type\Configurable
 {
-
     public function afterGetJsonConfig(
         \Magento\ConfigurableProduct\Block\Product\View\Type\Configurable $subject,
         $result
     ) {
         $jsonResult = json_decode($result, true);
-        
+
         $configurableProduct = $subject->getProduct();
-        
-        $name = $configurableProduct->getResource()->getAttributeRawValue($configurableProduct->getId(),['name'], 1);
-        $description = $configurableProduct->getResource()->getAttributeRawValue($configurableProduct->getId(),['description'], 1);
-        $short_description = $configurableProduct->getResource()->getAttributeRawValue($configurableProduct->getId(),['short_description'], 1);
-        
+
+        $name = $configurableProduct->getResource()->getAttributeRawValue($configurableProduct->getId(), ['name'], 1);
+        $description = $configurableProduct->getResource()->getAttributeRawValue($configurableProduct->getId(), ['description'], 1);
+        $short_description = $configurableProduct->getResource()->getAttributeRawValue($configurableProduct->getId(), ['short_description'], 1);
+
         $jsonResult['configurable_name'] =  $name;
         $jsonResult['configurable_description'] =  $description;
         $jsonResult['configurable_short_description'] =  $short_description;
 
         $jsonResult['skus'] = [];
         foreach ($subject->getAllowProducts() as $simpleProduct) {
-            $description = $simpleProduct->getResource()->getAttributeRawValue($simpleProduct->getId(),['description'], 1);
-            $short_description = $simpleProduct->getResource()->getAttributeRawValue($simpleProduct->getId(),['short_description'], 1);
+            $description = $simpleProduct->getResource()->getAttributeRawValue($simpleProduct->getId(), ['description'], 1);
+            $short_description = $simpleProduct->getResource()->getAttributeRawValue($simpleProduct->getId(), ['short_description'], 1);
             $jsonResult['skus'][$simpleProduct->getId()] = $simpleProduct->getSku();
             $jsonResult['names'][$simpleProduct->getId()] = $simpleProduct->getName();
             $jsonResult['descriptions'][$simpleProduct->getId()] = $description;
